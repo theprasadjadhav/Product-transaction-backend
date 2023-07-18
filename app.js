6fbdbbc44282bf11dd5885bc48ab7c0019c3a4f1
theprasadjadhav <prasadjadhav510@gmail.com>
2023-07-18 22:04:31 +0530

    error correction

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const mysql = require("mysql2");
 const axios = require("axios");
 const app = express();
 
-const months = { "january": 1, "february": 2, "march": 3, "april": 4, "may": 5, "june": 6, "july": 7, "august": 8, "september": 9, "octuby": 10, "november": 11, "december": 12 };
+const months = { "january": 1, "february": 2, "march": 3, "april": 4, "may": 5, "june": 6, "july": 7, "august": 8, "september": 9, "october": 10, "november": 11, "december": 12 };
 
 const pool = mysql.createPool({
   host: "127.0.0.1",
